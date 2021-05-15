d724eab9dbe38779c9845e2b4f5ed989717d6612
kush2-3 <71101590+kush2-3@users.noreply.github.com>
2021-05-15 15:42:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } = require("constants");
+
+const boo = document.getElementById("head");
+import swal from 'sweetalert';
+
+boo.addEventListener('click', () => {
+    alert("YOU ARE SPECIAL");
+
+
+})
